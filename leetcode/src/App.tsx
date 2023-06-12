eb731a6e477151ab5a77e6766e1a0307099466a1
Jasurbek Isokov <jasurbekisokov@Jasurbeks-MacBook-Pro.local>
2023-06-12 08:46:56 +0500

    Leetcode initial commit

---
+++ leetcode/src/App.tsx
@@ -0,0 +1,11 @@
+import ProblemAndSolution from "./ProblemAndSolution/ProblemAndSolution";
+
+function App(): JSX.Element {
+  return (
+    <>
+      <ProblemAndSolution />
+    </>
+  );
+}
+
+export default App;
---
+++ leetcode/src/ProblemAndSolution/ProblemAndSolution.tsx
@@ -0,0 +1,7 @@
+const ProblemAndSolution = (): JSX.Element => {
+  console.log("this");
+
+  return <h1>Problem and Solution</h1>;
+};
+
+export default ProblemAndSolution;
